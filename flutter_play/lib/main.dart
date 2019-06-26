8992dcc638b45753f779dc1cf75fd6cac86a81b2
LSViana <lv201122@gmail.com>
2019-06-25 21:23:25 -0300

    Started setState() test

--- flutter_play/lib/main.dart
+++ flutter_play/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_play/views/state-mgmt/state_mgmt_intro.dart';
+import 'package:flutter_play/views/state-mgmt/testing_setstate.dart';
 
 void main() {
   runApp(
-    StateMgmt()
+    SetStateTest()
   );
 }
