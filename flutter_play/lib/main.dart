84e576f9e5a9fc9dd7b0fc2cf2c078ae9f4388ac
LSViana <lv201122@gmail.com>
2019-07-06 23:19:32 -0300

    Created sample app to display SVG

--- flutter_play/lib/main.dart
+++ flutter_play/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_play/movie_bloc/app.dart';
+import 'package:flutter_play/svg/svg_app.dart';
 
 void main() {
   runApp(
-    MovieApp(),
+    SvgApp(),
   );
 }
