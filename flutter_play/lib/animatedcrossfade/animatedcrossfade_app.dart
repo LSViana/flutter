f7b6d0324abb0dee990151eee3757d6f0320a635
Lucas Viana <lucas.viana@MONSTER-APPLE.local>
2019-07-17 18:19:01 -0300

    Updating AnimatedCrossFadeApp

--- flutter_play/lib/animatedcrossfade/animatedcrossfade_app.dart
+++ flutter_play/lib/animatedcrossfade/animatedcrossfade_app.dart
@@ -7,7 +7,7 @@ class AnimatedCrossFadeApp extends StatefulWidget {
 }
 
 class _AnimatedCrossFadeAppState extends State<AnimatedCrossFadeApp> {
-  CrossFadeState _crossFadeState;
+  CrossFadeState _crossFadeState = CrossFadeState.showFirst;
 
   @override
   Widget build(BuildContext context) {
@@ -18,36 +18,47 @@ class _AnimatedCrossFadeAppState extends State<AnimatedCrossFadeApp> {
             'Animated Cross Fade App',
           ),
         ),
-        body: Card(
-          child: Container(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      _crossFadeState =
-                          _crossFadeState == CrossFadeState.showFirst
-                              ? CrossFadeState.showSecond
-                              : CrossFadeState.showFirst;
-                    });
-                  },
-                  child: Text('Expandable child'),
-                ),
-                AnimatedCrossFade(
-                  crossFadeState: _crossFadeState,
-                  duration: Duration(milliseconds: 300),
-                  firstChild: Container(
-                    height: 100,
-                    color: Colors.red,
+        body: GestureDetector(
+          onTap: () {
+            setState(() {
+              _crossFadeState = _crossFadeState == CrossFadeState.showFirst
+                  ? CrossFadeState.showSecond
+                  : CrossFadeState.showFirst;
+            });
+          },
+          child: Card(
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text('Expandable child'),
+                  AnimatedCrossFade(
+                    crossFadeState: _crossFadeState,
+                    duration: Duration(milliseconds: 300),
+                    firstChild: Container(
+                      // height: 100,
+                      color: Colors.red,
+                    ),
+                    secondChild: Container(
+                      // height: 200,
+                      color: Colors.orange,
+                      // Version 1
+                      // child: Row(
+                      //   mainAxisAlignment: MainAxisAlignment.center,
+                      //   children: <Widget>[
+                      //     Text('Expanded'),
+                      //   ],
+                      // ),
+                      // Version 2
+                      child: Container(
+                        alignment: Alignment.center,
+                        child: Text('Expanded')
+                      ),
+                    ),
                   ),
-                  secondChild: Container(
-                    height: 200,
-                    color: Colors.orange,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
--- flutter_play/lib/main.dart
+++ flutter_play/lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter_play/google_maps/google_maps_app.dart';
 import 'package:flutter_play/movie_bloc/app.dart';
 import 'package:flutter_play/pageview/pageview_app.dart';
 import 'package:flutter_play/svg/svg_app.dart';
+import 'package:flutter_play/views/state_management.dart';
 
 void main() {
   runApp(
